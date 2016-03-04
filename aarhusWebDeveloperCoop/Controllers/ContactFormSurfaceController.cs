0ce1771ae63fe8bec61fd1cea273dc44a305870a
Marina <juztmejuztme@yahoo.com>
2016-03-04 13:52:52 +0100

    added a contact form that works ;)

--- aarhusWebDeveloperCoop/Controllers/ContactFormSurfaceController.cs
+++ aarhusWebDeveloperCoop/Controllers/ContactFormSurfaceController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using Umbraco.Web.Mvc;
@@ -14,6 +15,36 @@ namespace aarhusWebDeveloperCoop.Controllers
         public ActionResult Index()
         {
             return PartialView("ContactForm", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model) {
+            //create a MailMessage object. it uses the System.Net.Mail namespace
+            MailMessage message = new MailMessage();
+
+            //set the email where the messages will be sent to
+            message.To.Add("juztmejuztme23@gmail.com");
+
+            //take what the user input and put it in the message that's going to be sent to you
+            message.Subject = model.Subject;
+            message.From = new MailAddress(model.Email, model.Name);
+            message.Body = model.Message;
+
+            //for Gmail emails
+            using (SmtpClient smtp = new SmtpClient()) {
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.EnableSsl = true;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.Credentials = new System.Net.NetworkCredential("juztmejuztme23@gmail.com", "vyfrxyanifccwdvk");
+                smtp.EnableSsl = true;
+
+                //send mail
+                smtp.Send(message);
+            }
+
+            return RedirectToCurrentUmbracoPage();
         }
     }
 }
