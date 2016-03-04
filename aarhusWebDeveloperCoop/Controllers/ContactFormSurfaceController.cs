9f67c97444b4434d8eddd1ef6fc4ef25105d93f5
Marina <juztmejuztme@yahoo.com>
2016-03-04 12:03:35 +0100

    Added Contact Us page

---
+++ aarhusWebDeveloperCoop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,19 @@
+﻿using aarhusWebDeveloperCoop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace aarhusWebDeveloperCoop.Controllers
+{
+    public class ContactFormSurfaceController : Umbraco.Web.Mvc.SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+    }
+}
---
+++ aarhusWebDeveloperCoop/ViewModels/ContactForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aarhusWebDeveloperCoop.ViewModels
+{
+    public class ContactForm
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
